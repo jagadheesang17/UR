27f6c215fbed87257bcb6c4d6a9b37593091d97c
jagadheesang17 <jagadheesang@peopleone.co>
2025-09-30 15:47:01 +0530

    Add files via upload

---
+++ tests/admin/User_Creation/WC001_Configure_the_request_walletcard_delivery_in_config_file.spec.ts
@@ -0,0 +1,19 @@
+import { test } from "../../../customFixtures/expertusFixture";
+
+test.describe(`Configure_the_request_walletcard_delivery_in_config_file`, async () => {
+    test.describe.configure({ mode: "serial" });
+test(`Configure the wallet card delivery in configuration file`, async ({adminHome}) => {
+    test.info().annotations.push(
+        { type: `Author`, description: `Tamilvanan` },
+        { type: `TestCase`, description: `Enable company login alone in site settings` },
+        { type: `Test Description`, description: `Enable company login alone in site settings` }
+
+    );
+    await adminHome.loadAndLogin("SUPERADMIN");
+    await adminHome.menuButton();
+    await adminHome.siteAdmin();
+    await adminHome.walletCardConfiguration();
+}
+)
+}
+)
---
+++ tests/admin/User_Creation/WC002_Verify_the_admin_checked_request_walletcard_delivery_in_learnerside.spec.ts
@@ -0,0 +1,50 @@
+import { URLConstants } from '../../../constants/urlConstants';
+import { test } from '../../../customFixtures/expertusFixture';
+import { FakerData } from '../../../utils/fakerUtils';
+const courseAdmin: any = FakerData.getUserId();
+
+
+test.describe(`Configure_the_request_walletcard_delivery_in_config_file`, async () => {
+    test.describe.configure({ mode: "serial" });
+
+test(`Create an user`, async ({ adminHome, createUser }) => {
+    test.info().annotations.push(
+        { type: `Author`, description: `Vidya` },
+        { type: `TestCase`, description: `Create user for portal 1` },
+        { type: `Test Description`, description: `Verify that user is created for portal 1` }
+    );   
+        await adminHome.loadAndLogin("SUPERADMIN");
+        await adminHome.menuButton();
+        await adminHome.people();
+        await adminHome.user();
+        await createUser.clickCreateUser();    
+        await createUser.verifyCreateUserLabel();    
+        await createUser.enter("first_name", FakerData.getFirstName());
+        await createUser.enter("last_name", FakerData.getLastName());
+        await createUser.enter("username", courseAdmin);
+        await createUser.enter("user-password", "Welcome1@");
+        await createUser.selectTimeZone("USA","Pacific Standard");
+        await createUser.checkRequestWalletCardDelivery();
+        await createUser.clickSave();               
+        await createUser.verifyUserCreationSuccessMessage();
+    }
+
+    
+)
+test(`Verify the error message displayed when the created learner attempts to select the "Request Wallet Card Delivery" option without completing at least one course.`, async ({ learnerHome,profile}) => {
+    test.info().annotations.push(
+        { type: `Author`, description: `Tamilvanan` },
+        { type: `TestCase`, description: `Verify that user address validation functionality working as expected` },
+        { type: `Test Description`, description: `Verifying user address validation functionality working as expected` }
+    );   
+        await learnerHome.clearBrowserCache(URLConstants.leanerURL);
+        await learnerHome.basicLogin(courseAdmin, "Portal");
+        await learnerHome.termsAndConditionScroll();
+        await profile.clickProfile();
+        await profile.clickOneProfile();
+        await profile.verifyRequestPhysicalWalletCardDeliveryCheckbox();
+        
+    
+    }
+)
+})
